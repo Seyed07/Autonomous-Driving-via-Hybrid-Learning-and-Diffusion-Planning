69218516e8ac45e1db1604e89663aa3222eba530
Seyed Ahmad Hosseini <hosseiniahmad07@gmail.com>
2025-10-22 14:00:46 +0330

    Delete CONTACT_FOR_CODE.py

--- CONTACT_FOR_CODE.py
+++
@@ -1,9 +0,0 @@
-The source code for this project is not publicly available by default.
-Access is granted upon request and approval.
-
-If you wish to access or use the code, please contact the author via email or open an issue in this repository, stating your affiliation and intended use.
-
-Email: [seyedahmad.hosseini@aut.ac.ir]
-Once your request is approved, you will be granted access to the private repository.
-
-Thank you for your understanding.
